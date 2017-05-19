08ab445daceb46c87d27d8f741784c08137d1f96
emilyjf <flanagan.emily.j@gmail.com>
2017-05-18 21:33:33 -0500

    Thursday

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Contact.create!([
+  {first_name: "Becky", last_name: "Flanagan", email: "bf@gmail.com", phone: "212-313-2431", middle_name: nil, bio: nil},
+  {first_name: "Marc", last_name: "Flanagan", email: "mf@gmail.com", phone: "212-313-2433", middle_name: nil, bio: nil},
+  {first_name: "John", last_name: "Flanagan", email: "jff@gmail.com", phone: "212-313-2439", middle_name: nil, bio: nil},
+  {first_name: "Mimi", last_name: "Cukier", email: "cm@gmail.com", phone: "444-555-6666", middle_name: nil, bio: nil},
+  {first_name: "Tash", last_name: "Torres", email: "td@gmail.com", phone: "111-222-3333", middle_name: nil, bio: nil}
+])
