2012a20bfe5d240e3ab77694139eef9e32d2194b
emilyjf <flanagan.emily.j@gmail.com>
2017-05-22 21:26:27 -0500

    Monday

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518230942) do
+ActiveRecord::Schema.define(version: 20170523013600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(version: 20170518230942) do
     t.datetime "updated_at",  null: false
     t.string   "middle_name"
     t.string   "bio"
+  end
+
+  create_table "group_contacts", force: :cascade do |t|
+    t.integer  "contact_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-Contact.create!([
-  {first_name: "Becky", last_name: "Flanagan", email: "bf@gmail.com", phone: "212-313-2431", middle_name: nil, bio: nil},
-  {first_name: "Marc", last_name: "Flanagan", email: "mf@gmail.com", phone: "212-313-2433", middle_name: nil, bio: nil},
-  {first_name: "John", last_name: "Flanagan", email: "jff@gmail.com", phone: "212-313-2439", middle_name: nil, bio: nil},
-  {first_name: "Mimi", last_name: "Cukier", email: "cm@gmail.com", phone: "444-555-6666", middle_name: nil, bio: nil},
-  {first_name: "Tash", last_name: "Torres", email: "td@gmail.com", phone: "111-222-3333", middle_name: nil, bio: nil}
-])
+# Contact.create!([
+#   {first_name: "Becky", last_name: "Flanagan", email: "bf@gmail.com", phone: "212-313-2431", middle_name: nil, bio: nil},
+#   {first_name: "Marc", last_name: "Flanagan", email: "mf@gmail.com", phone: "212-313-2433", middle_name: nil, bio: nil},
+#   {first_name: "John", last_name: "Flanagan", email: "jff@gmail.com", phone: "212-313-2439", middle_name: nil, bio: nil},
+#   {first_name: "Mimi", last_name: "Cukier", email: "cm@gmail.com", phone: "444-555-6666", middle_name: nil, bio: nil},
+#   {first_name: "Tash", last_name: "Torres", email: "td@gmail.com", phone: "111-222-3333", middle_name: nil, bio: nil}
+#               ])
+
+# Group.create!([
+#               {"friends", "family", "business"}
+#               ])
+
+Group.create!(name: "friends")
+Group.create!(name: "family")
+Group.create!(name: "business")
+
+contact_ids = Contact.all
+group_ids = Group.pluck(:id)
+
+contact_ids.each do |c_id|
+  selected_group_ids = group_ids.sample(rand(1..3))
+
+  selected_group_ids.each do |g_id|
+  GroupContact.create!(c_id: 1, g_id: 1)
+  end
+end
